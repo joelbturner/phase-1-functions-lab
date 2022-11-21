14082f449b4315c5545b4b7b8735719fb212aa92
Joel Turner <joelbturner.mb@gmail.com>
2022-11-21 18:24:18 -0500

    commit tree 0d0fb2198b82a723ada26aadb8e3ff28af9eb541

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    if (someValue >= 42){
+        return someValue - 42;
+    }
+    else 
+    return 42 - someValue;
+}
+
+function distanceFromHqInFeet(someValue){
+   
+   return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start >= destination){
+        return (start - destination) *264;
+    }
+    else {
+        return (destination - start) * 264;
+    }
+}
+function calculatesFarePrice(start, destination){
+    const money = distanceTravelledInFeet(start, destination);
+    if (money > 2500){
+        return 'cannot travel that far'
+    }
+    else if (money <= 2499 && money >= 2000) {
+        return 25;
+    }
+    else if(money > 400 && money <= 2000) {
+        return ((money - 400) * 0.02)
+    }
+    else if(money <= 400){
+        return 0
+    }
+}
+
+// 2 cents per foot
+// 264 feet
